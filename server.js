ea0320ff3af2c3c35069614f9d42e84a43824c39
James <mirageapache@gmail.com>
2023-06-02 10:23:58 +0800

    revise env

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const cors = require('cors');
 const body_parser = require('body-parser');
 const { response } = require('express');
 
-// const client_id = process.env.CLIENT_ID;
-// const client_secret = process.env.CLIENT_SECRET;
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
-const client_id = "56101bc6f878daff5d1e";
-const client_secret = "57bee343d8fede6a3969c03ec087d730c1bd59f5";
-
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
 
 let app = express();
 app.use(cors());
